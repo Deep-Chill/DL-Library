31e657d30e6dfe5519cdce4e94eb0c2cef901337
Deep-Chill <nbukhari96@gmail.com>
2023-07-28 00:26:15 -0700

    Completed FC Layer.

--- Layers/DenseLayer.py
+++ Layers/DenseLayer.py
@@ -1,2 +1,32 @@
-class Layer():
-    pass
+from .Layers import Layer
+import numpy as np
+
+class FullyConnectedLayer(Layer):
+    def __init__(self, input_size, output_size):
+        self.weights = np.random.rand(input_size, output_size) - 0.5
+        self.biases = np.random.rand(1, output_size) - 0.5
+
+    def forward_propagation(self, input):
+        self.input = input
+        self.output = np.dot(input, self.weights) + self.biases
+
+        return self.output
+
+    def backward_propagation(self, output_error, learning_rate):
+        input_error = np.dot(output_error, self.weights.T)
+        weights_error = np.dot(self.input.T, output_error)
+
+        self.weights -= learning_rate * weights_error
+        self.bias -= learning_rate * output_error
+        return input_error
+
+class ActivationLayer(Layer):
+    def __init__(self, activation_function, activation_function_derivative):
+        self.activation_function = activation_function
+        self.activation_function_derivative = activation_function_derivative
+
+    def forward_propagation(self, input):
+        input = self.input
+        output = self.activation_function(input)
+
+    def backward_propagation(self, output_error, learning_rate):
