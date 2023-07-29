eef5e4daa50becc49fdb4f4ac69347f2df92f132
Deep-Chill <nbukhari96@gmail.com>
2023-07-28 22:47:47 -0700

    Close to completing NN

--- Layers/DenseLayer.py
+++ Layers/DenseLayer.py
@@ -41,53 +41,67 @@ class ActivationLayer(Layer):
         return self.activation_derivative(self.input) * output_error
 
 class NNCompiler():
-    def __init__(self, input_size, n_layers, activation_function, activation_derivative, input_data, model_architecture, epochs):
+    def __init__(self, input_size, n_layers, activation_function, activation_derivative, input_data, model_architecture,
+                 epochs, labels):
         self.input_size = input_size
         self.n_layers = n_layers
         self.activation_function = activation_function
         self.activation_derivative = activation_derivative
         self.input_data = input_data
         self.architecture = model_architecture
-        self.layer_order = self.architecture.layer_order
-        self.layer_sizes = self.architecture.layer_sizes
-        self.layer_outputs = self.architecture.layer_outputs
+        self.layer_type = self.architecture.layer_type
+        self.layer_size = self.architecture.layer_size
+        self.layer_output_size = self.architecture.layer_output_size
         self.epochs = epochs
+        self.list_of_layers = []
+        for i in range(len(self.architecture)):
+            if self.layer_type[i] == 'FC':
+                fc_layer = FullyConnectedLayer(self.layer_size[i], self.layer_output_size[i])
+                self.list_of_layers.append(fc_layer)
+            elif self.layer_type[i] == 'AL':
+                al_layer = ActivationLayer(self.activation_function, self.activation_derivative)
+                self.list_of_layers.append(al_layer)
+        self.list_of_outputs = []
+        self.labels = labels
+
+        # if isinstance(self.list_of_layers[-1], FullyConnectedLayer):
+        #     if len(self.labels) != self.list_of_layers[-1].output:
+        #         raise ValueError("The size of labels does not match the size of output of the final layer in the nn")
+        # elif isinstance(self.list_of_layers[-1], ActivationLayer):
+        #     if len(self.labels) != self.list_of_layers[-2].output:
+        #         raise ValueError("The size of labels does not match the size of output of the final layer in the nn")
+
 
     def first_layer(self):
-        pass
+        self.list_of_outputs.append(self.list_of_layers[0].forward_propagation(self.input_data))
 
     def forward_propagate(self):
-        for i in range(self.n_layers):
-            
+        for i, layers in enumerate(self.list_of_layers[1:]):
+            if isinstance(layers, FullyConnectedLayer):
+                self.list_of_outputs.append(layers.forward_propagation(self.list_of_outputs[i-1]))
+            elif isinstance(layers, ActivationLayer):
+                self.list_of_outputs.append(layers.forward_propagation(self.list_of_outputs[i-1]))
 
 
 
 
     def backward_propagate(self):
         for i in range(self.n_layers):
+            pass
 
 
 
 
 # A model architecture class to define the order of layers in the model.
 # The model architecture will be a list with dictionaries in the shape:
-# [{'type': 'FC', 'layer_size': 10, 'output_size': 5}, {'type': 'AL', 'size': 10, 'output_size': 10}]
+# [{'type': 'FC', 'layer_size': 10, 'output_size': 5}, {'type': 'AL', 'size': 10, 'output_size': 10, 'activation_function': 'sigmoid'}]
 # Each item will be a layer. Each item has details about the layer.
 class ModelArchitecture():
     def __init__(self, input):
         self.architecture = input
-        self.layer_order = [i('type') for i in self.architecture]
-        self.layer_sizes = [i('layer_size') for i in self.architecture]
-        self.layer_outputs = [i('output_size') for i in self.architecture]
-
-
-
-
-
-
-
-
-
+        self.layer_type = [i('type') for i in self.architecture]
+        self.layer_size = [i('layer_size') for i in self.architecture]
+        self.layer_output_size = [i('output_size') for i in self.architecture]
 
 
 
